ab93f3d3c81a0717042239a1d421af20743c216c
Mitja Maren <mico.maren@gmail.com>
2016-03-10 15:36:44 +0000

    Narejeno

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -12,6 +12,14 @@ window.addEventListener('load', function() {
 	
 			//TODO: 
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
+			if (cas>0){
+				casovnik.innerHTML=cas-1;
+			}
+			else {
+				var naziv=opomnik.querySelector(".naziv_opomnika").innerHTML;
+				alert(naziv);
+				document.querySelector("#opomniki").removeChild(opomnik);
+			}
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
 		}
 	}
@@ -27,7 +35,7 @@ window.addEventListener('load', function() {
 	var zacetekOpomnika=function(event){
 		var ime=document.querySelector("#naziv_opomnika").value;
 		var cas=document.querySelector("#cas_opomnika").value;
-		document.querySelector("#opomniki").innerHTML+="<div class='opomnik'><div class='naziv_opomnika'>"+ime+"</div><div class='cas_opomnika'> Opomnik čez <span>"+cas+"</span> sekund.</div></div>";
+		document.querySelector("#opomniki").innerHTML+="<div class='opomnik rob senca'><div class='naziv_opomnika'>"+ime+"</div><div class='cas_opomnika'> Opomnik čez <span>"+cas+"</span> sekund.</div></div>";
 	}
 	
 	document.querySelector("#prijavniGumb").addEventListener('click',izvediPrijavo);
