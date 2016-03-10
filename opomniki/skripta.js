e8279bc4b90ec892bf169be3a769bc22ad753fc9
Mitja Maren <mico.maren@gmail.com>
2016-03-10 14:54:48 +0000

    Implementirana prijava.

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -17,4 +17,13 @@ window.addEventListener('load', function() {
 	}
 	setInterval(posodobiOpomnike, 1000);
 	
+	var izvediPrijavo=function(event){
+		var ime=document.querySelector("#uporabnisko_ime").value;
+		document.querySelector("#uporabnik").innerHTML=ime;
+		document.querySelector(".pokrivalo").style.display="none";
+		
+	}
+	
+	document.querySelector("#prijavniGumb").addEventListener('click',izvediPrijavo);
 });
+
